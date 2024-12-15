8a4eaa55c4f065072ad053c759e74e0d245332b1
raoufffffff <raouf1999hamoudi@gmail.com>
2024-12-16 00:29:51 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Features from "./components/Features"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -11,6 +12,7 @@ function App() {
     <Navbar />
     <Hero />
     <About />
+    <Features />
    </main>
   )
 }
